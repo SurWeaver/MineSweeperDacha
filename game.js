a4bf0b74dda83050e969d42a85024e29b6fae6ea
Alexander Tkachenko <xander.tka4enko@gmail.com>
2024-10-09 13:34:51 +0300

    Добавлен выбор сложности и редактор в меню.

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
-import { Scene } from "./scene";
+import { DEFAULT_DIFFICULTIES, DIF_BEGINNER, DIF_NAME, GameDifficulty } from "./difficulty.js";
+import DifficultyChanger from "./gameObjects/difficulty_changer.js";
+import Field from "./gameObjects/field.js";
+import StartGameButton from "./gameObjects/start_game_button.js";
+import { Scene } from "./scene.js";
 
 export const SCENE_MENU = 0;
 export const SCENE_GAME = 1;
@@ -8,6 +12,9 @@ export const game = {
 	ctx: null,
 	cvs: null,
 	currentScene: null,
+
+	customDifficulty: new GameDifficulty(9, 9, 10),
+	selectedDifficulty: DEFAULT_DIFFICULTIES[DIF_BEGINNER],
 
 
 	initialize: function(canvas, context) {
@@ -24,10 +31,38 @@ export const game = {
 	},
 
 	changeScene: function(scene) {
+		if (this.currentScene)
+			delete this.currentScene;
+
 		switch (scene) {
 		case SCENE_MENU:
 			this.currentScene = new Scene();
+
+			for (let i = 0; i < DEFAULT_DIFFICULTIES.length; i++)
+				this.currentScene.addActor(
+					new StartGameButton(DIF_NAME[i], DEFAULT_DIFFICULTIES[i], 20, 20 + i * 60,
+						() => {
+							this.selectedDifficulty = DEFAULT_DIFFICULTIES[i];
+							this.changeScene(SCENE_GAME);
+						})
+					);
+
+			this.currentScene.addActor(
+				new StartGameButton("Своя", this.customDifficulty, 20, 200,
+					() => {
+						if (!this.customDifficulty.isValid())
+							return;
+
+						this.selectedDifficulty = this.customDifficulty;
+						this.changeScene(SCENE_GAME);
+					}
+				)
+			);
+
+			this.currentScene.addActor(new DifficultyChanger(20, 300, this.customDifficulty));
+			
 			break;
+
 		case SCENE_GAME:
 			break;
 		}
