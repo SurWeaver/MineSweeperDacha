fa7b3e85f3fae0d5bac5cb8192a86934fa190d5e
Alexander Tkachenko <xander.tka4enko@gmail.com>
2024-10-09 12:20:40 +0300

    Исправил this-ошибки в GameDifficulty, добавил константы по стандартным сложностям.

--- difficulty.js
+++ difficulty.js
@@ -4,11 +4,36 @@ export class GameDifficulty {
 		this.height = height;
 		this.bombCount = bombCount;
 
-		if (getBombPercent() >= 1)
+		if (this.getBombPercent() >= 1)
 			throw Error("Wrong bomb count!");
 	}
 
 	getBombPercent() {
-		return bombCount / width * height;
+		return this.bombCount / (this.width * this.height);
+	}
+
+	isValid() {
+		let percent = this.getBombPercent();
+		return this.width >= 1 && this.height >= 1 &&
+			percent > 0 && percent < 1.0;
 	}
 }
+
+
+export const DEFAULT_DIFFICULTIES = [
+	new GameDifficulty(9, 9, 10),
+	new GameDifficulty(16, 16, 40),
+	new GameDifficulty(30, 16, 99)
+];
+
+export const DIF_BEGINNER = 0;
+export const DIF_INTERMEDIATE = 1;
+export const DIF_EXPERT = 2;
+export const DIF_CUSTOM = 3;
+
+export const DIF_NAME = [
+	"Новичок",
+	"Любитель",
+	"Профессионал",
+	"Своя"
+];
